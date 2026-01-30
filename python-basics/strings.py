69eb50988f4fe3418a0cd8120501feeeff5fb942
mutable <amirbakiev502@gmail.com>
2026-01-30 22:24:13 +0500

    code upload

---
+++ python-basics/strings.py
@@ -0,0 +1,15 @@
+text = "Hello world!"
+
+print(text.upper())           
+print(text.lower())           
+print(len(text))              
+print(text[0:6])              
+
+name = "Amir"
+language = "Python"
+message = f"{name} learns {language}"
+print(message)                
+
+sentence = "i love coding"
+print(sentence.capitalize())  
+    
